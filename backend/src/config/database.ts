e4fe4df23553c620c65041bbdf475e17148ea7ff
zahra <zahraaalihasan279@gmail.com>
2025-04-04 16:02:22 +0300

    fixed after review

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,20 +1,15 @@
 import { Sequelize } from "sequelize";
 import { config } from "./env";
 
-const databaseUrl = config.databaseUri;
-console.log(config)
-if (!databaseUrl) {
-    throw new Error("DATABASE_URL is not defined");
-}
 const sequelize = new Sequelize(config.databaseUri, {
   dialect: "postgres",
   protocol: "postgres",
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,  
-  //     rejectUnauthorized: false, 
-  //   },
-  // },
+  dialectOptions: {
+    ssl: {
+      require: true,  
+      rejectUnauthorized: false, 
+    },
+  },
   logging: false
 });
 
